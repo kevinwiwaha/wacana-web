12a16332b67f0826003dc39c9d0b40b43d36e045
kevinwiwaha <kevinwiwaha@gmail.com>
2022-11-09 10:21:11 +0700

    init nextJS

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+const supabase = require('@supabase/supabase-js')
 export default function handler(req, res) {
+  const { data, error } = await supabase
+  .from('countries')
+  .select()
   res.status(200).json({ name: 'John Doe' })
 }
